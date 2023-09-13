2962d6619c4675faeb5daa70c7dad681c5ae8697
TheuTheuutra <105327246+TheuTheuutra@users.noreply.github.com>
2023-09-12 22:18:29 -0300

    commit dos exemplos e exercicios

---
+++ projeto_js/ex02.js
@@ -0,0 +1,13 @@
+function celsius()
+{
+    var valor = parseFloat (document.getElementById('valor').value);
+    document.getElementById('resultado').innerHTML = valor;
+    
+}
+
+function fa()
+{
+    var valor = parseFloat (document.getElementById('valor').value);
+    document.getElementById('resultado').innerHTML = valor;
+    
+}
---
+++ projeto_js/script.js
@@ -0,0 +1,12 @@
+alert("Eu sou o primeiro a aparecer");
+
+function carregou()
+{
+    alert("Página carregou");
+}
+
+function clicou()
+{
+    alert("Algúem clicou no botão");
+    document.getElementById('aqui').innerHTML=("professor Mário");
+}
